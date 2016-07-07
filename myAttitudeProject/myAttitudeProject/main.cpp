0e6c4156fdbebd11df6321393d2855266d997c3b
TAKUMI-ABE <basara.ysb@gmail.com>
2016-07-07 18:27:09 +0900

    add Eigen and define matrix for EKF

--- myAttitudeProject/myAttitudeProject/main.cpp
+++ myAttitudeProject/myAttitudeProject/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream> // ifstream, ofstream
 #include <sstream> // istringstream
 
+#include <Eigen/Core>
 #include "myUtility.h"
 
 const std::string fileName = "../../data/IMU_sample_100Hz.txt";
@@ -12,6 +13,50 @@ const double FIRST_TIME = 0.01;
 const double IMU_LPF = 0.07; // èoóÕÇÉGÉNÉZÉãÇ≈í≠ÇﬂÇƒÇ¢Ç¢ä¥Ç∂ÇÃílÇëIë
 const std::vector<double> coordinateModifyFactor{1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 const std::vector<double> BIAS{0.264562, -0.1776, -0.30055, -0.02727, 0.010064, 0.009732}; // ê√é~ÇµÇƒÇ¢ÇÈ0Å`5sÇÃïΩãœÇ≈éZèo
+
+const double INIT_R = 0.1;
+const double INIT_Q = 0.1;
+const double INIT_P = 0.01;
+const std::vector<double> INIT_X { 0.0, 0.0, 0.0, 1.0 };
+const int SIZE_X = 4;
+const int SIZE_Y = 3;
+const int SIZE_F = SIZE_X;
+const int SIZE_H = SIZE_Y;
+const int SIZE_G_ROW = SIZE_X; // ROW : çs(â°)
+const int SIZE_G_COL = SIZE_Y; // COL : óÒ(èc)
+const int SIZE_A_ROW = SIZE_X;
+const int SIZE_A_COL = SIZE_X;
+const int SIZE_C_ROW = SIZE_Y;
+const int SIZE_C_COL = SIZE_X;
+const int SIZE_P = SIZE_X;
+const int SIZE_Q = SIZE_X;
+const int SIZE_R = SIZE_Y;
+
+template<typename T>
+class myKalmanFilter {
+private:
+	// Eigen::Matrix<T, Rows(â°óÒÇ¢Ç≠Ç¬), Cols(ècóÒÇ¢Ç≠Ç¬)> matrix_name;
+	Eigen::Matrix<T, 4, 1> xHat;		// Estimated value of the state valiable
+	Eigen::Matrix<T, 4, 1> xHatMinus;	// Advance estimate of the state valiable
+	Eigen::Matrix<T, 3, 1> y;			// Observation value
+	Eigen::Matrix<T, 4, 1> f;			// System Matrix
+	Eigen::Matrix<T, 3, 1> h;			// Transformation matrix from state valiable to observation value
+	Eigen::Matrix<T, 4, 4> A;			// Linearized f
+	Eigen::Matrix<T, 4, 4> AT;			// Transposition of A
+	Eigen::Matrix<T, 4, 3> C;			// Liniarized h
+	Eigen::Matrix<T, 3, 4> CT;			// Transposition of C
+	Eigen::Matrix<T, 4, 3> g;			// Kalman gain
+	Eigen::Matrix<T, 4, 4> P;			// Covariance matrix
+	Eigen::Matrix<T, 4, 4> PMinus;		// Advance covariance matrix
+	Eigen::Matrix<T, 4, 4> Q;			// Variance matrix of w
+	Eigen::Matrix<T, 3, 3> R;			// Variance matrix of v
+public:
+	myKalmanFilter(const std::vector<T>& x, const T p, const T q, const T r);
+	~myKalmanFilter() {};
+
+	Eigen::Matrix<T, 4, 1> getXHat() const { return xHat; };
+};
+
 
 template<typename T>
 class u {
@@ -39,6 +84,12 @@ std::ostream& operator<<(std::ostream& os, const u<T>& udata);
 int main(int argc, char** argv)
 {
 	u<double> input(NUM_OF_IMU_DATA);
+	myKalmanFilter<double> kalman(INIT_X, INIT_P, INIT_Q, INIT_R);
+
+	/*
+	std::cout << kalman.getXHat() << std::endl;
+	while (1) {}
+	*/
 
 	std::ifstream file;
 	try {
@@ -71,6 +122,41 @@ int main(int argc, char** argv)
 	}
 
 	return 0;
+}
+
+// constructor of myKalmanFilter
+template<typename T>
+myKalmanFilter<T>::myKalmanFilter(const std::vector<T>& x, const T p, const T q, const T r) 
+{
+	xHat << x[0], 
+		    x[1], 
+		    x[2], 
+		    x[3];
+
+	P << p  , 0.0, 0.0, 0.0,
+		 0.0, p  , 0.0, 0.0,
+		 0.0, 0.0, p  , 0.0,
+		 0.0, 0.0, 0.0  , p;
+
+	P << q  , 0.0, 0.0, 0.0,
+		 0.0, q  , 0.0, 0.0,
+		 0.0, 0.0, q  , 0.0,
+		 0.0, 0.0, 0.0, q  ;
+
+	R << r  , 0.0, 0.0,
+		 0.0, r  , 0.0,
+		 0.0, 0.0, r  ;
+
+	xHatMinus.setZero();
+	y.setZero();
+	f.setZero();
+	h.setZero();
+	PMinus.setZero();
+	g.setZero();
+	A.setZero();
+	AT.setZero();
+	C.setZero();
+	CT.setZero();
 }
 
 // constructor of u
