3e1bee1a536eab66d9317674ac88c71c43386096
TAKUMI-ABE <basara.ysb@gmail.com>
2016-07-08 11:44:04 +0900

    impliment algorithm but doesnt work

--- myAttitudeProject/myAttitudeProject/main.cpp
+++ myAttitudeProject/myAttitudeProject/main.cpp
@@ -5,11 +5,14 @@
 #include <sstream> // istringstream
 
 #include <Eigen/Core>
+#include <Eigen/LU>
 #include "myUtility.h"
 
 const std::string fileName = "../../data/IMU_sample_100Hz.txt";
 const int NUM_OF_IMU_DATA = 6;
 const double FIRST_TIME = 0.01;
+const double DT = 0.01;
+const double GRAV_ACC = 9.80665;
 const double IMU_LPF = 0.07; // èoóÕÇÉGÉNÉZÉãÇ≈í≠ÇﬂÇƒÇ¢Ç¢ä¥Ç∂ÇÃílÇëIë
 const std::vector<double> coordinateModifyFactor{1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 const std::vector<double> BIAS{0.264562, -0.1776, -0.30055, -0.02727, 0.010064, 0.009732}; // ê√é~ÇµÇƒÇ¢ÇÈ0Å`5sÇÃïΩãœÇ≈éZèo
@@ -33,32 +36,6 @@ const int SIZE_Q = SIZE_X;
 const int SIZE_R = SIZE_Y;
 
 template<typename T>
-class myKalmanFilter {
-private:
-	// Eigen::Matrix<T, Rows(â°óÒÇ¢Ç≠Ç¬), Cols(ècóÒÇ¢Ç≠Ç¬)> matrix_name;
-	Eigen::Matrix<T, 4, 1> xHat;		// Estimated value of the state valiable
-	Eigen::Matrix<T, 4, 1> xHatMinus;	// Advance estimate of the state valiable
-	Eigen::Matrix<T, 3, 1> y;			// Observation value
-	Eigen::Matrix<T, 4, 1> f;			// System Matrix
-	Eigen::Matrix<T, 3, 1> h;			// Transformation matrix from state valiable to observation value
-	Eigen::Matrix<T, 4, 4> A;			// Linearized f
-	Eigen::Matrix<T, 4, 4> AT;			// Transposition of A
-	Eigen::Matrix<T, 4, 3> C;			// Liniarized h
-	Eigen::Matrix<T, 3, 4> CT;			// Transposition of C
-	Eigen::Matrix<T, 4, 3> g;			// Kalman gain
-	Eigen::Matrix<T, 4, 4> P;			// Covariance matrix
-	Eigen::Matrix<T, 4, 4> PMinus;		// Advance covariance matrix
-	Eigen::Matrix<T, 4, 4> Q;			// Variance matrix of w
-	Eigen::Matrix<T, 3, 3> R;			// Variance matrix of v
-public:
-	myKalmanFilter(const std::vector<T>& x, const T p, const T q, const T r);
-	~myKalmanFilter() {};
-
-	Eigen::Matrix<T, 4, 1> getXHat() const { return xHat; };
-};
-
-
-template<typename T>
 class u {
 private:
 	T t;
@@ -80,6 +57,37 @@ public:
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const u<T>& udata);
 
+template<typename T>
+class myKalmanFilter {
+private:
+	// Eigen::Matrix<T, Rows(â°óÒÇ¢Ç≠Ç¬), Cols(ècóÒÇ¢Ç≠Ç¬)> matrix_name;
+	Eigen::Matrix<T, 4, 1> xHat;		// Estimated value of the state valiable
+	Eigen::Matrix<T, 4, 1> xHatMinus;	// Advance estimate of the state valiable
+	Eigen::Matrix<T, 3, 1> y;			// Observation value
+	Eigen::Matrix<T, 4, 1> f;			// System Matrix
+	Eigen::Matrix<T, 3, 1> h;			// Transformation matrix from state valiable to observation value
+	Eigen::Matrix<T, 4, 4> A;			// Linearized f
+	Eigen::Matrix<T, 4, 4> AT;			// Transposition of A
+	Eigen::Matrix<T, 4, 3> C;			// Liniarized h
+	Eigen::Matrix<T, 3, 4> CT;			// Transposition of C
+	Eigen::Matrix<T, 4, 3> g;			// Kalman gain
+	Eigen::Matrix<T, 4, 4> P;			// Covariance matrix
+	Eigen::Matrix<T, 4, 4> PMinus;		// Advance covariance matrix
+	Eigen::Matrix<T, 4, 4> Q;			// Variance matrix of w
+	Eigen::Matrix<T, 3, 3> R;			// Variance matrix of v
+	Eigen::Matrix<T, 4, 4> I;			// Unit Matrix (4*4) 
+public:
+	myKalmanFilter(const std::vector<T>& x, const T p, const T q, const T r);
+	~myKalmanFilter() {};
+
+	Eigen::Matrix<T, 4, 1> getXHat() const { return xHat; };
+
+	void prediction(const u<T>& input);
+	void filter(const u<T>& input);
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const myKalmanFilter<T>& k);
 
 int main(int argc, char** argv)
 {
@@ -108,17 +116,18 @@ int main(int argc, char** argv)
 
 	while (std::getline(file, buf))
 	{
-		// string data -> vector
+		// string data -> vector, bias filter, LPF
 		input.setUData(buf, coordinateModifyFactor);
-
-		// bias filter
 		input.setImuDataBias(BIAS);
-		
-		// LPF
 		input.setLPF(FIRST_TIME, IMU_LPF);
-		//input.coutData();
 
-		std::cout << input << std::endl;
+		//std::cout << input << std::endl;
+
+		// kalman filter
+		kalman.prediction(input);
+		kalman.filter(input);
+
+		std::cout << input.getT() << "," << kalman << std::endl;
 	}
 
 	return 0;
@@ -147,6 +156,12 @@ myKalmanFilter<T>::myKalmanFilter(const std::vector<T>& x, const T p, const T q,
 		 0.0, r  , 0.0,
 		 0.0, 0.0, r  ;
 
+	I << 1.0, 0.0, 0.0, 0.0,
+		 0.0, 1.0, 0.0, 0.0,
+		 0.0, 0.0, 1.0, 0.0,
+		 0.0, 0.0, 0.0, 1.0;
+
+	// fill other matrix 0
 	xHatMinus.setZero();
 	y.setZero();
 	f.setZero();
@@ -157,6 +172,78 @@ myKalmanFilter<T>::myKalmanFilter(const std::vector<T>& x, const T p, const T q,
 	AT.setZero();
 	C.setZero();
 	CT.setZero();
+}
+
+template<typename T>
+void myKalmanFilter<T>::prediction(const u<T>& input)
+{
+	std::vector<T> imu,w,q;
+	imu = input.getImuDataLPF();
+	w.push_back(static_cast<T>(0));
+	for (int i = 3; i < 6; ++i)
+		w.push_back(imu[i]); // w[1]~w[3]
+
+	A <<      1.0    ,  0.5*DT*w[3], -0.5*DT*w[2], 0.5*DT*w[1],
+		 -0.5*DT*w[3],      1.0    ,  0.5*DT*w[1], 0.5*DT*w[2],
+		  0.5*DT*w[2], -0.5*DT*w[1],      1.0    , 0.5*DT*w[3],
+		 -0.5*DT*w[1], -0.5*DT*w[2], -0.5*DT*w[3],     1.0    ;
+
+	f = A * xHat;
+
+	xHatMinus = f;
+
+	q.push_back(static_cast<T>(0));
+	for (int i = 0; i < 4; ++i)
+		q.push_back(xHatMinus[i]); // q[1]~q[4]
+
+	CT <<  2 * q[3] * GRAV_ACC, -2 * q[4] * GRAV_ACC, 2 * q[1] * GRAV_ACC, -2 * q[2] * GRAV_ACC,
+		   2 * q[4] * GRAV_ACC,  2 * q[3] * GRAV_ACC, 2 * q[2] * GRAV_ACC,  2 * q[1] * GRAV_ACC,
+		  -2 * q[1] * GRAV_ACC, -2 * q[2] * GRAV_ACC, 2 * q[3] * GRAV_ACC,  2 * q[4] * GRAV_ACC;
+
+	PMinus = A * P * A.transpose() + Q;
+
+}
+
+template<typename T>
+void myKalmanFilter<T>::filter(const u<T>& input) {
+	std::vector<T> imu, a, q;
+	imu = input.getImuDataLPF();
+	a.push_back(static_cast<T>(0));
+	for (int i = 0; i < 3; ++i)
+		a.push_back(imu[i]); // a[1]~a[3]
+	q.push_back(static_cast<T>(0));
+	for (int i = 0; i < 4; ++i)
+		q.push_back(xHatMinus[i]); // q[1]~q[4]
+
+	C = CT.transpose();
+
+	Eigen::Matrix<T, 3, 3> TMP;
+	TMP = CT * PMinus * C + R;
+
+	g = PMinus * C * TMP.inverse();
+
+	y << a[1],
+		 a[2],
+		 a[3];
+
+	h << 2 * (q[3] * q[1] - q[2] * q[4]) * GRAV_ACC,
+		 2 * (q[2] * q[3] - q[1] * q[4]) * GRAV_ACC,
+		 2 * (q[3] * q[3] - q[1] * q[1] - q[2] * q[2] + q[4] * q[4]) * GRAV_ACC;
+
+	xHat = xHatMinus + g * (y - h);
+
+
+
+	P = { I - g * CT } *PMinus;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const myKalmanFilter<T>& k) {
+	os << k.getXHat()[0] << ","
+		<< k.getXHat()[1] << ","
+		<< k.getXHat()[2] << ","
+		<< k.getXHat()[3];
+	return os;
 }
 
 // constructor of u
